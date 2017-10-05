c2428863e2707c24235f9caf5228e86730ef19d1
Emanuel <eduardo.velarde@pucp.pe>
2017-10-05 03:11:08 -0500

    Clientes.java NuevoCliente.java TransaccionVenta.java NuevaTransaccionVenta.java

--- Cilix/src/cilix/Cilix.java
+++ Cilix/src/cilix/Cilix.java
@@ -10,12 +10,9 @@ package cilix;
  * @author Emanuel ji
  */
 public class Cilix {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
