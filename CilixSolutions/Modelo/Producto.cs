c287746c27bf94a097276b2c953995d7e52006d4
Walter Reyes Olórtegui <w.reyeso@pucp.pe>
2017-10-05 09:28:39 -0500

    Capa Modelo: Producto, Proveedor FormLogueo

---
+++ CilixSolutions/Modelo/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Producto
+    {
+        string _nombre;
+        int _cantidad;
+        double _precio;
+        string _estado;
+        string _categoria;
+
+        public Producto(string nombre, int cantidad, double precio, string estado, string categoria)
+        {
+            _nombre = nombre;
+            _cantidad = cantidad;
+            _precio = precio;
+            _estado = estado;
+            _categoria = categoria;
+        }
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public int Cantidad { get => _cantidad; set => _cantidad = value; }
+        public double Precio { get => _precio; set => _precio = value; }
+        public string Estado { get => _estado; set => _estado = value; }
+        public string Categoria { get => _categoria; set => _categoria = value; }
+    }
+}
---
+++ CilixSolutions/Vista/Logueo.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Vista
+{
+    partial class Logueo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.lbContraseña = new System.Windows.Forms.Label();
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Location = new System.Drawing.Point(64, 87);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lbUsuario.TabIndex = 0;
+            this.lbUsuario.Text = "Usuario";
+            // 
+            // lbContraseña
+            // 
+            this.lbContraseña.AutoSize = true;
+            this.lbContraseña.Location = new System.Drawing.Point(64, 148);
+            this.lbContraseña.Name = "lbContraseña";
+            this.lbContraseña.Size = new System.Drawing.Size(61, 13);
+            this.lbContraseña.TabIndex = 1;
+            this.lbContraseña.Text = "Contraseña";
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Location = new System.Drawing.Point(164, 225);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpezar.TabIndex = 2;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(164, 87);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(164, 148);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // Logueo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 294);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnEmpezar);
+            this.Controls.Add(this.lbContraseña);
+            this.Controls.Add(this.lbUsuario);
+            this.Name = "Logueo";
+            this.Text = "Logueo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.Label lbContraseña;
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+    }
+}
