30cf99800d85b08b969f06389349d13bde749d9e
alulab14 <alulab14@inf.pucp.edu.pe>
2017-11-22 21:23:37 -0500

    Reportes en Java

---
+++ Cilix/src/Reportes/Reportes.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reportes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+/**
+ *
+ * @author alulab14
+ */
+public class Reportes {
+    
+    public Reportes(){
+        
+    }
+    
+    public void productosMasVendidos(){
+        try{
+            //registrar el Driver 
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //establecer la conexión
+            Connection conn = DriverManager.getConnection("jdbc:sqlserver://200.16.7.140; databaseName=inf282g4;"
+                    + "integratedSecurity=false;username=inf282g4; password=LnyBcOhGWyvFVtBp;");
+            JasperReport reporte = (JasperReport)JRLoader.loadObjectFromFile(Reportes.class.getResource("/Reportes/reporteProductos.jasper").getFile());
+            JasperPrint print  = JasperFillManager.fillReport(reporte,null,conn);
+            JasperViewer v = new JasperViewer(print);
+            v.setTitle("Clientes");
+            v.setVisible(true);
+            
+        }
+        catch(Exception e){
+            
+        }
+        
+    }
+    
+    public void mejoresProveedores(){
+        try{
+            //registrar el Driver 
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //establecer la conexión
+            Connection conn = DriverManager.getConnection("jdbc:sqlserver://200.16.7.140; databaseName=inf282g4;"
+                    + "integratedSecurity=false;username=inf282g4; password=LnyBcOhGWyvFVtBp;");
+            JasperReport reporte = (JasperReport)JRLoader.loadObjectFromFile(Reportes.class.getResource("/Reportes/reporteProveedores.jasper").getFile());
+            JasperPrint print  = JasperFillManager.fillReport(reporte,null,conn);
+            JasperViewer v = new JasperViewer(print);
+            v.setTitle("Clientes");
+            v.setVisible(true);
+        }
+        catch(Exception e){
+            
+        }        
+    }    
+}
